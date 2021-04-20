bf8599e0024255131e0481ec5bc8379164022b06
Marcos Luciano <marcos3dt@gmail.com>
2021-04-20 14:12:19 -0300

    modificando os aquivos para receber a lista de filmes da web

--- app/src/main/java/com/alura/listfilme/api/MovieApi.kt
+++ app/src/main/java/com/alura/listfilme/api/MovieApi.kt
@@ -1,11 +1,12 @@
 package com.alura.listfilme.api
 
 import com.alura.listfilme.Movie
+import retrofit2.Call
 import retrofit2.http.GET
 
 interface MovieApi {
 
     //Chamada para API LISTA DE FILMES
     @GET("natanfelipe/FilmesFlixJson/master/moviesList")
-    fun getAllMovie(): List<Movie>
+    fun getAllMovie(): Call<List<Movie>>
 }
--- app/src/main/java/com/alura/listfilme/view/model/MovieListViewModel.kt
+++ app/src/main/java/com/alura/listfilme/view/model/MovieListViewModel.kt
@@ -4,8 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.alura.listfilme.Movie
+import com.alura.listfilme.api.MovieRestApi
 
 class MovieListViewModel: ViewModel() {
+    private val movieRestApi = MovieRestApi()
     private val listOfMovie = arrayListOf(
         Movie(
             id = 0,
